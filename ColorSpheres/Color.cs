b8e6703f00e9391a3319d5b821f33569899d91c6
Hugo Silva <hugofigueira.dev@gmail.com>
2024-04-10 16:52:31 +0100

    Adding construtors and methods to class Color

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Drawing;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+        private Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        private static Color ShowColor(byte red, byte green, byte blue, byte alpha)
+        {
+            return new Color(red, green, blue, alpha);
+        }
+
+        private byte Red_method
+        {
+            get {return red;}
+            set {red = value;}
+        }
+
+        private byte Blue_method
+        {
+            get {return blue;}
+            set {blue = value;}
+        }
+
+        private byte Green_method
+        {
+            get {return green;}
+            set {green = value;}
+        }
+
+        private byte Alpha_method
+        {
+            get {return alpha;}
+            set {alpha = value;}
+        }
+
+
+        
+    }
+}
